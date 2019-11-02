6360a4cd4e3d9733aaf52531a7beee1c695daa5d
Travis Evans <travis909@gmail.com>
2019-11-02 12:24:37 -0400

    changes for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-  .
+  # Settings specified here will take precedence over those in config/application.rb
 
 
   config.cache_classes = true
